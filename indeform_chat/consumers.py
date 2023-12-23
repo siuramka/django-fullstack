f19e362727f2fd4bcbf077514deda6b676feb53f
siuramka <marius.kamarauskas2001@gmail.com>
2023-12-23 21:13:53 +0200

    functionality - added chat backend

--- indeform_chat/consumers.py
+++ indeform_chat/consumers.py
@@ -4,96 +4,89 @@ from asgiref.sync import sync_to_async
 from channels.auth import login, logout
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User
 from rest_framework_simplejwt import exceptions
 from rest_framework_simplejwt.tokens import AccessToken, Token
+
+from indeform_base.models import CustomUser, ChatRoom
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        room_name = self.scope['url_route']['kwargs']['room_name']
+        room_id = self.scope['url_route']['kwargs']['room_id']
         headers = dict(self.scope['headers'])
         token = headers[b'bearer'].decode()
         decoded_token = await self.decode_jwt(token)
-        print(f" payload: {decoded_token.attributes}")
-        user = await self.get_user_from_token(decoded_token)
-        print(f"user {user}")
 
+        try:
+            user = await self.get_user_from_token(decoded_token)
+        except User.DoesNotExist:
+            await self.send({"close": True})
+            return
 
-
-
-
-        return;
-        if user.is_authenticated:
-            self.room_name = self.scope['url_route']['kwargs']['room_name']
-            # self.room_group_name = 'chat_%s' % self.room_name
-            self.room_group_name = f"chat_{self.room_name}"
-
-            # Join room group
-            await self.channel_layer.group_add(
-                self.room_group_name,
-                self.channel_name
-            )
-            await self.accept()
-
-        else:
+        try:
+            sender_participant_user = await self.get_participating_sender_user(room_id, user.id)
+        except ChatRoom.DoesNotExist:
+            await self.send({"close": True})
+        except CustomUser.DoesNotExist:
             await self.send({"close": True})
 
-    async def disconnect(self, close_code):
-        """
-        Disconnect from channel
 
-        :param close_code: optional
-        """
+        self.room_group_name = f"chat_{room_id}"
+
+        #add websocket connection to channel
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     async def receive(self, text_data):
-        """
-        Receive messages from WebSocket
-
-        :param text_data: message
-        """
-
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        username = text_data_json['username']
-        profile_pic = text_data_json['profile_pic']
-        room = text_data_json['room']
-
+        sender_email = text_data_json['sender_email']
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username,
-                'room': room,
+                'sender_email': sender_email
             }
         )
 
     async def chat_message(self, event):
-        """
-        Receive messages from room group
-
-        :param event: Events to pick up
-        """
         message = event['message']
-        username = event['username']
-        profile_pic = event['profile_pic']
-        room = event['room']
+        sender_email = event['sender_email']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message,
-            'username': username,
-            'profile_pic': profile_pic,
-            'room': room,
+            'text': message,
+            'sender_email': sender_email
         }))
 
     @database_sync_to_async
     def decode_jwt(self, token):
         return AccessToken(token)
+
+    @database_sync_to_async
+    def get_user_from_token(self, token):
+        user_id = token.payload['user_id']
+        email = token.payload['email']
+        username = token.payload['username']
+        return CustomUser.objects.get(pk=user_id, email=email, username=username)
+
+    @database_sync_to_async
+    def get_chatroom(self, chatroom_id):
+        return ChatRoom.objects.get(pk=chatroom_id)
+
+    @database_sync_to_async
+    def get_participating_sender_user(self, chatroom_id, sender_user_id):
+        return ChatRoom.objects.get(pk=chatroom_id).participants.get(id=sender_user_id)
--- indeform_chat/routing.py
+++ indeform_chat/routing.py
@@ -6,5 +6,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/(?P<room_name>[^/]+)$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/(?P<room_id>[^/]+)$', consumers.ChatConsumer.as_asgi()),
 ]
